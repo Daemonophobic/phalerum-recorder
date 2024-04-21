63a3b04e17407ccaceefdd03e263a33e77a4cb3f
Marijn van Dijk <marijn@stickybits.red>
2024-04-21 21:22:47 +0200

    Updated utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def log(message):
-    print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {message}")
+    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {message}")
 
 def getCurrentMinute():
     return datetime.now().strftime("%M")
